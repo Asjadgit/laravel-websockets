0333fdac8d0b9dc4fa10f8b9a2961bc9e49eb201
Asjad Naveed <115245920+Asjadgit@users.noreply.github.com>
2024-11-12 17:02:27 +0500

    Add files via upload
    storage added

---
+++ backend/storage/framework/views/a66f7f1919515a4ffe06def889798521.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <?php echo app('Illuminate\Foundation\Vite')('resources/js/app.js'); ?>
+</body>
+<script>
+    setTimeout(() => {
+        window.Echo.channel('testing-channel')
+        .listen('NotificationEvent', (e)=>{
+            console.log(e);
+        })
+    }, 200);
+</script>
+</html>
+<?php /**PATH E:\React_laravel\TestingWebSocket\backend\resources\views/welcome.blade.php ENDPATH**/ ?>
